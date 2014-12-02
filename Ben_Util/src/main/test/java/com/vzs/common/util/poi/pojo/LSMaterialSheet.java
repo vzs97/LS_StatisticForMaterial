7f0fc97e5afd1f78b25bf005c877ec8978240e0e
Ben Yao <byao@coupang.com>
2014-12-02 18:12:24 +0800

    update annotation

--- Ben_Util/src/main/test/java/com/vzs/common/util/poi/pojo/LSMaterialSheet.java
+++ Ben_Util/src/main/test/java/com/vzs/common/util/poi/pojo/LSMaterialSheet.java
@@ -1,6 +1,9 @@
 package com.vzs.common.util.poi.pojo;
 
+import com.google.common.collect.Lists;
 import lombok.Data;
+
+import java.util.List;
 
 /**
  * Created by ben.yao on 12/1/2014.
@@ -10,5 +13,5 @@ import lombok.Data;
 public class LSMaterialSheet {
 	@BCell(row=7,column = 2,types = BCell.TYPES.STRING)
 	String m7;
-
+    List<LSMaterialRow> lsMaterialRowList = Lists.newArrayList();
 }
